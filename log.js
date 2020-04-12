7d305ad26f184eeef667df6dd5ad96d4ce81fdc0
Zhichao Shi <Shiz4756@uwec.edu>
2020-04-12 02:48:40 -0500

    update-new

---
+++ log.js
@@ -0,0 +1,11 @@
+function validateForm () {
+    var x = document.forms['myForm']['fname'].value
+    var pw=document.forms['myForm']['password'].value
+    if (x != '000000' && pw!='000000') {
+      alert('Wrong log in messsage!')
+      return false;
+    }
+    else{
+        window.open("index.html");
+    }
+  }
---
+++ sign-up.js
@@ -0,0 +1,13 @@
+function validateForm () {
+    var x = document.forms['myForm']['fname'].value
+    var pw=document.forms['myForm']['password'].value
+    var cpw=document.forms['myForm']['cpassword'].value
+    if (x != '000000' && pw!='000000' && cpw!="000000") {
+      alert('Wrong log in messsage!')
+      return false;
+    }
+    else{
+        alert('Successfully!')
+        window.open("index.html");
+    }
+  }
